a4b0a471053ae8f726bac1069242adcfdb6a6cb6
Léon Smit <lee_smit@live.nl>
2016-04-12 21:41:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+jcons
+=========
+
+jcons allows you to generate inline svg strings from your .svg files. This allows you to use inline svg icons in your (Javascript) webapplication.
+
+## Installation
+
+  npm install jsons --save
+
+## Release History
+
+* 0.1.0 Initial release
---
+++ index.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+'use strict'
+
+// include dependencies
+var _ = require('lodash');
+var fs = require('fs');
+var path = require('path');
+var svgo = require('svgo');
+var jsonfile = require('jsonfile');
+var mkdirp = require('mkdirp');
+var svgoGenerator = new svgo();
+
+// read jconfig.json
+var config = JSON.parse(fs.readFileSync('jconfig.json', 'utf8'));
+
+// set config vars
+var source = config.source || {};
+var sourceDir = source.dir || 'src/';
+var output = config.output || {};
+var outputDir = output.dir || 'dist/';
+var outputSpaces = output.spaces || 2;
+var outputName = output.fileName || 'output';
+
+var dirpath = path.resolve(__dirname, sourceDir);
+
+var generateJcons = function() {
+    fs.readdir(dirpath, function(error, files) {
+        if (error) throw error;
+        var generateOutputFile = function(outputObject) {
+            console.log('jcons: completed ' + outputDir + outputName + '.json');
+            mkdirp(outputDir, function(error) {
+                if (error) throw error;
+                jsonfile.writeFile(outputDir + outputName + '.json', outputObject, { spaces: outputSpaces }, function (error) {
+                    if (error) throw error;
+                });
+            });
+        };
+        var iconsObject     = {};
+        var totalIcons      = files.length;
+        var currentIcon     = 0;
+        _.each(files, function(fileName, index) {
+            // check for .svg extension
+            if(!_.endsWith(fileName, '.svg')) {
+                totalIcons--;
+                return;
+            }
+
+            // remmeber icon name without extension
+            var iconKey = _.trimEnd(fileName, '.svg');
+
+            // read each file
+            fs.readFile(path.resolve(__dirname, sourceDir + fileName), 'utf8', function(error, svgData) {
+                if (error) throw error;
+
+                // optimize svg file
+                svgoGenerator.optimize(svgData, function(result) {
+                    var svgString = result.data;
+                    // add svg string to icons object
+                    iconsObject[iconKey] = svgString;
+
+                    // increment current icon to keep track of progress
+                    currentIcon++;
+                    console.log('jcons: generated [' + currentIcon + '/' + totalIcons + '] ' + iconKey);
+                    if(currentIcon === totalIcons) generateOutputFile(iconsObject);
+                });
+
+            });
+        });
+
+    });
+};
+generateJcons();
